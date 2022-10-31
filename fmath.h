3792c7fe565828ed12c0ea9d96413e5050e2db6e
Stanley Goodwin <sfg99709aktech@gmail.com>
2022-10-30 23:14:35 -0400

    Updated FMath
    Removed the 6x6 Determinant Function.
    WARNING THIS WILL CAUSE THE PROGRAM TO CEASE FUNCTIONING!

--- fmath.h
+++ fmath.h
@@ -1,25 +1,13 @@
-/**
+/*
  * File:	fmath.h
- * Brief:	Provides fast math operations.
- *
  * Author:	Stanley Goodwin
- * Contact:	sfg99709akwork@gmail.com
- *
- * Creation Date: 6/21/2022
- * Last Modified: 7/31/2022
+ * Provides a set of fast math approximation functions.
  */
 #pragma once
 #ifndef FMATH_H
 #define FMATH_H
 
-
-// Fast square roots
-double f_inv_sqrt(double number);  // Fast inverse square root
-double f_sqrt(double number);      // Fast square root
-
-// Using cramers rule to find inverse matrix values
-void cramer(double matrix[6][6], double output[6], double coeff[6]);
-void gaussian_elimination(double augmented_matrix[6][7], double coeff[6]);
-
+double f_inv_sqrt(double number);
+double f_sqrt(double number);
 
 #endif FMATH_H
